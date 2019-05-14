5ae7eeb8558a1f386b2b5e3d8ceb87a5b33fca48
jiangshuibing <1436026890@qq.com>
2019-05-14 22:38:20 +0800

    前端页面

--- src/main/resources/static/web/admin/js/index.js
+++ src/main/resources/static/web/admin/js/index.js
@@ -1,41 +1,28 @@
-$(document).ready(function(){
-    $(".btn_next").mouseup(function(){
-        $(".btn_next").css("border","0");
-        $(".btn_next").css("color","#fff");
-        $(".btn_next").css("border",0);
-    });
-    $(".btn_next").mousedown(function(){
-        $(".btn_next").css("border-color","#096148");
-        $(".btn_next").css("color","#66bab7");
-        $(".btn_next").css("border",0);
-    });
+$(function(){	
+		// 导航左侧栏js效果 start
+		$(".pullDownList li").hover(function(){
+			$(".yMenuListCon").fadeIn();
+			var index=$(this).index(".pullDownList li");
+			if (!($(this).hasClass("menulihover")||$(this).hasClass("menuliselected"))) {
+				$($(".yBannerList")[index]).css("display","block").siblings().css("display","none");
+				$($(".yBannerList")[index]).removeClass("ybannerExposure");
+				setTimeout(function(){
+				$($(".yBannerList")[index]).addClass("ybannerExposure");
+				},60)
+			}else{	
+			}
+			$(this).addClass("menulihover").siblings().removeClass("menulihover");
+				$(this).addClass("menuliselected").siblings().removeClass("menuliselected");
+			$($(".yMenuListConin")[index]).fadeIn().siblings().fadeOut();
+		},function(){
+			
+		})
+		$(".pullDown").mouseleave(function(){
+			$(".yMenuListCon").fadeOut();
+			$(".yMenuListConin").fadeOut();
+			$(".pullDownList li").removeClass("menulihover");
 
-
-    //调用一言api
-    $(".btn_next").on("click",function(){
-        getQuote();
-    });
-
-    var content="Blessed is the man, who having nothing to say, abstains from giving wordy evidence of the fact.";
-    var author="Paul Erdos";
-    var getQuote = function(){
-        $.getJSON("https://sslapi.hitokoto.cn/?encode=json",function(json){
-            content = json["hitokoto"];
-            author = json["from"];
-            //console.log(content+"_"+author);
-
-            $(".quote-content").html(content);
-            $(".quote-author").html("——"+author);
-        });
-    }
-
-    getQuote();
-
-
-    $(".shareQQ").on("click",function(){
-        console.log(content+author);
-        window.open('http://sns.qzone.qq.com/cgi-bin/qzshare/cgi_qzshare_onekey?url=http://www.baidu.com&desc='+content+'——'+author+'&title=吟游佳句&summary=我发现了一句很漂亮的话，快来看看吧&pics=&site=bshare');
-    });
-
-
-});
+		})
+		// 导航左侧栏js效果  end
+		
+	})
