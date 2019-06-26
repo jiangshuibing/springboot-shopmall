81bf76cca03b95997db06637323197cd8c2c305d
jiangshuibing <jiangshuibing@zhuojianchina.com>
2019-06-26 15:10:22 +0800

    '修改地址'

--- src/main/java/cn/ucmed/interceptor/filter/DruidStatViewServlet.java
+++ src/main/java/cn/ucmed/interceptor/filter/DruidStatViewServlet.java
@@ -8,7 +8,7 @@ import com.alibaba.druid.support.http.StatViewServlet;
 @SuppressWarnings("serial")
 @WebServlet(urlPatterns = "/druid/*",
         initParams={
-                @WebInitParam(name="allow",value="192.168.0.113,127.0.0.1"),// IP白名单 (没有配置或者为空，则允许所有访问)
+                @WebInitParam(name="allow",value="192.168.32.164,127.0.0.1"),// IP白名单 (没有配置或者为空，则允许所有访问)
                 @WebInitParam(name="deny",value="192.168.16.111"),// IP黑名单 (存在共同时，deny优先于allow)
                 @WebInitParam(name="loginUsername",value="shanhy"),// 用户名
                 @WebInitParam(name="loginPassword",value="shanhypwd"),// 密码
